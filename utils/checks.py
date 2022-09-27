c8b2e9bbb4694d71b6f7b87bb69197ecdeaf269f
FrancescoNegri <francesco.negri@outlook.com>
2022-09-27 14:55:34 +0200

    Update check all settings method

--- utils/checks.py
+++ utils/checks.py
@@ -1,11 +1,19 @@
 import os
 
 
-def check_all_setting(var, sep: str, suffix: str, path: str):
+def check_all_setting(var, sep: str, path: str, prefix: str = None, suffix: str = None):
+    if (prefix is None) and (suffix is None):
+        raise ValueError(
+            'prefix and suffix parameters cannot be None at the same time.')
+
     if (var == 'all') or (var == ['all']):
         var = []
         for file in os.listdir(path):
-            if file.endswith(suffix):
+            prefix_none = (prefix is None) and (file.endswith(suffix))
+            suffix_none = (suffix is None) and (file.startswith(prefix))
+            prefix_suffix = ((prefix is not None) and (file.startswith(prefix))) and ((suffix is not None) and (file.endswith(suffix)))
+
+            if  prefix_none or suffix_none or prefix_suffix:
                 _, _var = file.split(sep)
                 _var = _var.split('_')[0]
                 _var = _var.replace('-', '')
