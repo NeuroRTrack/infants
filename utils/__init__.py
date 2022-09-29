3a452ef6fc44c3496530c1f98f7d2efbbbb9ae40
FrancescoNegri <francesco.negri@outlook.com>
2022-09-29 17:52:20 +0200

    Add kwargs check

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from .warnings import IgnoreWarnings
 from .io import create_output_dir, get_filename
-from .checks import check_all_setting
+from .checks import check_all_setting, check_kwargs_list
 
-__all__ = ['IgnoreWarnings', 'check_all_setting',
+__all__ = ['IgnoreWarnings', 'check_all_setting', 'check_kwargs_list',
            'create_output_dir', 'get_filename']
--- utils/checks.py
+++ utils/checks.py
@@ -25,3 +25,23 @@ def check_all_setting(var, sep: str, path: str, prefix: str = None, suffix: str 
     var.sort()
 
     return var
+
+def check_kwargs_list(kwargs_list, **kwargs):
+    for kwarg in kwargs_list:
+        kwargs[kwarg['key']] = kwargs.get(kwarg['key'], kwarg['default'])
+
+        if kwarg['type'] is not None:
+            if (kwargs.get(kwarg['key']) is None) and (kwarg['default'] is None):
+                pass
+            elif kwargs.get(kwarg['key']) is None:
+                pass
+            else:
+                try:
+                    if ((kwarg['type'] == list) or (kwarg['type'] == tuple)) and (type(kwargs.get(kwarg['key'])) is str):
+                        kwargs[kwarg['key']] = [kwargs.get(kwarg['key'])]
+
+                    kwargs[kwarg['key']] = kwarg['type'](kwargs.get(kwarg['key']))
+                except:
+                    raise TypeError("'" + kwarg['key'] + "' expected to be '" + kwarg['type'].__name__ + "', received '" + str(type(kwargs.get(kwarg['key'])).__name__) + "'")
+
+    return kwargs
