161fdcd37e6d4ef59ea9500bf721e0ce10c120c0
GaiaBurlando <gaia.burlando@libero.it>
2022-10-04 16:44:43 +0200

    Add multibar plots

--- visualization/__init__.py
+++ visualization/__init__.py
@@ -1,3 +1,4 @@
 from .circadian import plot_circadian_cycle
+from .multibar import plot_multi_bar
 
-__all__ = ['plot_circadian_cycle']
+__all__ = ['plot_circadian_cycle', 'plot_multi_bar']
---
+++ visualization/multibar.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from utils import check_kwargs_list
+
+
+def _parse_kwargs(x, **kwargs):
+    kwargs_list = [
+        {'key': 'ax', 'default': None, 'type': None},
+        {'key': 'boxoff', 'default': True, 'type': bool},
+        {'key': 'color', 'default': [None for _ in x], 'type': tuple},
+        {'key': 'dpi', 'default': 100, 'type': float},
+        {'key': 'figsize', 'default': (6, 3), 'type': tuple},
+        {'key': 'labels', 'default': None, 'type': tuple},
+        {'key': 'legend_anchor', 'default': None, 'type': tuple},
+        {'key': 'linewidth', 'default': 0.25, 'type': float},
+        {'key': 'num', 'default': None, 'type': str},
+        {'key': 'title', 'default': 'Multibar Plot', 'type': str},
+        {'key': 'xlabel', 'default': None, 'type': str},
+        {'key': 'xlim', 'default': None, 'type': tuple},
+        {'key': 'xticks', 'default': None, 'type': tuple},
+        {'key': 'ylabel', 'default': None, 'type': str},
+        {'key': 'ylim', 'default': None, 'type': tuple},
+        {'key': 'yticks', 'default': None, 'type': tuple}
+    ]
+    kwargs = check_kwargs_list(kwargs_list, **kwargs)
+
+    return kwargs
+
+def plot_multi_bar(x, data, **kwargs):
+    kwargs = _parse_kwargs(x, **kwargs)
+
+    if kwargs.get('ax') is None:
+        plt.figure(num=kwargs.get('num'), figsize=kwargs.get('figsize'), dpi=kwargs.get('dpi'))
+        ax = plt.gca()
+    else:
+        ax = kwargs.get('ax')
+    
+    for idx in range(np.shape(data)[0]):
+        ax.bar(x, data[idx], color=kwargs.get('color')[idx], edgecolor=kwargs.get('color')[idx], bottom=np.sum(data[:idx], axis=0) if idx != 0 else None)
+
+    ax.set_title(kwargs.get('title'))
+    ax.set_xlabel(kwargs.get('xlabel'))
+    ax.set_ylabel(kwargs.get('ylabel'))
+
+    if kwargs.get('xticks') is not None: ax.set_xticks(kwargs.get('xticks'))
+    if kwargs.get('yticks') is not None: ax.set_yticks(kwargs.get('yticks'))
+
+    ax.set_xlim(kwargs.get('xlim'))
+    ax.set_ylim(kwargs.get('ylim'))
+
+    if kwargs.get('boxoff') is True:
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+
+    if kwargs.get('labels') is not None: ax.legend(kwargs.get('labels'), loc='upper right', fontsize='small', bbox_to_anchor=kwargs.get('legend_anchor'))
+
+    return
