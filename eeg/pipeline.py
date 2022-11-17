e6a699025ace71b74c3193c93564bc54e0636b70
FrancescoNegri <francesco.negri@outlook.com>
2022-11-17 16:12:03 +0100

    Update eeg pipeline

--- eeg/pipeline.py
+++ eeg/pipeline.py
@@ -9,6 +9,28 @@ from .metrics import get_iPLV, get_PLV_mean
 from .preprocessing import preprocess_data, concat_epochs, get_epochs_from_annotations
 
 
+def get_adjacent_epochs_idxs(epochs, sfreq, description='Wake', window_duration=180):
+    adjacent_idxs = []
+
+    window_size = np.round(window_duration * sfreq)
+
+    descriptions = epochs.to_data_frame()['condition'].values.tolist()
+
+    start_idx = 0 if descriptions[0] == description else None
+
+    for idx, _description in enumerate(descriptions):
+        if _description != description:
+            start_idx = None
+        elif _description == description:
+            if start_idx is None:
+                start_idx = idx
+        
+        if (start_idx is not None) and (idx - start_idx >= window_size):
+            adjacent_idxs.append(list(range(start_idx, idx)))
+            start_idx = None
+
+    return np.matrix(adjacent_idxs)
+
 def run(settings):
     path = settings['general']['dataset_dir']
 
@@ -58,30 +80,61 @@ def run(settings):
         if (epochs is not None) and (len(epochs) > 0):
             for _ses in ses:
                 epochs = concat_epochs(data[_ses])
-
-                QS_epoch = [np.concatenate(epochs['QuietSleep']._data, axis=1)]
-                AS_epoch = [np.concatenate(epochs['ActiveSleep']._data, axis=1)]
+                sfreq = epochs.info['sfreq']
                 
-                print(np.shape(QS_epoch))
-                print(np.shape(AS_epoch))
+                QS_idxs = get_adjacent_epochs_idxs(epochs, sfreq, description='QuietSleep')
+                print(QS_idxs.shape)
 
-                freqs = np.linspace(1, 20, num=20)
+                data = np.array([(np.concatenate(epochs.get_data(), axis=1))])
+                data = mne.time_frequency.tfr_array_morlet(data, sfreq, [2]).squeeze()
+                print(type(data))
+                print(data.shape)
+                
+                QS_data = np.squeeze([data[:, window_idxs] for window_idxs in QS_idxs])
+                QS_data = np.moveaxis(QS_data, [0], [2])
+                print(type(QS_data))
+                print(QS_data.shape)
 
-                QS_iPLV = get_iPLV(QS_epoch, 512, freqs)
-                AS_iPLV = get_iPLV(AS_epoch, 512, freqs)
+                # temp = []
+                # for window in QS_data:
+                #     QS_iPLV = get_iPLV(window)
+                #     temp.append(QS_iPLV)
+                #     plt.figure()
+                #     plt.clf()
+                #     sns.heatmap(QS_iPLV[:, :, 0], cmap='viridis')
+                #     plt.show()
 
-                QS_iPLV_mean = get_PLV_mean(QS_iPLV)
-                AS_iPLV_mean = get_PLV_mean(AS_iPLV)
+                # temp = np.mean(temp, axis=0)
+                # print(np.shape(temp))
+                # plt.figure()
+                # plt.clf()
+                # sns.heatmap(temp[:, :, 0], cmap='viridis')
+                # plt.show()
 
-                plt.figure()
-                plt.plot(freqs, QS_iPLV_mean, freqs, AS_iPLV_mean)
-                plt.show()
-                plt.legend(['QS', 'AS'])
 
-                plt.figure()
-                plt.clf()
-                sns.heatmap(QS_iPLV[:, :, 0], cmap='viridis')
-                plt.show()
+                # QS_epoch = [np.concatenate(epochs['QuietSleep']._data, axis=1)]
+                # AS_epoch = [np.concatenate(epochs['ActiveSleep']._data, axis=1)]
+                
+                # print(np.shape(QS_epoch))
+                # print(np.shape(AS_epoch))
+
+                # freqs = np.linspace(1, 20, num=20)
+
+                # QS_iPLV = get_iPLV(QS_epoch, 512, freqs)
+                # AS_iPLV = get_iPLV(AS_epoch, 512, freqs)
+
+                # QS_iPLV_mean = get_PLV_mean(QS_iPLV)
+                # AS_iPLV_mean = get_PLV_mean(AS_iPLV)
+
+                # plt.figure()
+                # plt.plot(freqs, QS_iPLV_mean, freqs, AS_iPLV_mean)
+                # plt.show()
+                # plt.legend(['QS', 'AS'])
+
+                # plt.figure()
+                # plt.clf()
+                # sns.heatmap(QS_iPLV[:, :, 0], cmap='viridis')
+                # plt.show()
 
             # for event_id in list(epochs.event_id.keys()):
             #     fig = epochs[event_id].plot_psd(
