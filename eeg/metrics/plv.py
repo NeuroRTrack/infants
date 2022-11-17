d4fa90cda5a9355f2ed1cd462c18148ccb9ebbca
FrancescoNegri <francesco.negri@outlook.com>
2022-11-17 16:32:43 +0100

    Fix PLV module

--- eeg/metrics/plv.py
+++ eeg/metrics/plv.py
@@ -2,25 +2,6 @@ import mne
 import numpy as np
 import numpy.typing as npt
 
-# def get_cPLV(data, sfreq, freqs):
-#     morlet = mne.time_frequency.tfr_array_morlet(data, sfreq, freqs).squeeze()
-#     n_channels, n_freqs, _ = morlet.shape
-#     phase = np.angle(morlet)
-#     cPLV = np.zeros([n_channels, n_channels, n_freqs], dtype=complex)
-
-#     for freq_idx in range(n_freqs):
-#         for ch_1 in range(n_channels - 1):
-#             for ch_2 in range(ch_1 + 1, n_channels):
-#                 dphase = phase[ch_1, freq_idx, :] - phase[ch_2, freq_idx, :]
-#                 cPLV[ch_1, ch_2, freq_idx] = np.mean(np.exp(1j*dphase))
-
-#         cPLV[:, :, freq_idx] = cPLV[:, :, freq_idx] + cPLV[:, :, freq_idx].T
-        
-#     diag_idxs = np.diag_indices_from(cPLV[:, :, 0])
-#     cPLV[diag_idxs[0], diag_idxs[1], :] = 1
-
-#     return cPLV
-
 
 def get_cPLV(A: npt.ArrayLike, B: npt.ArrayLike = None):
     '''
@@ -45,18 +26,17 @@ def get_cPLV(A: npt.ArrayLike, B: npt.ArrayLike = None):
     '''
     if B is None:
         B = np.copy(A)
-    
+
     _, n_samples_A = A.shape
     _, n_samples_B = B.shape
 
     if n_samples_A == n_samples_B:
         n_samples = n_samples_A
 
-    A_norm = np.divide(A, abs(A))
-    B_norm = np.divide(B, abs(B))
+        A_norm = np.divide(A, abs(A))
+        B_norm = np.divide(B, abs(B))
 
-    cPLV = np.divide(np.matmul(A_norm, np.conjugate(B_norm.T)), n_samples)
-
+        cPLV = np.divide(np.matmul(A_norm, np.conjugate(B_norm.T)), n_samples)
     else:
         raise ValueError('The number of samples of the matrices is different.')
 
@@ -105,6 +85,7 @@ def get_iPLV(A: npt.ArrayLike, B: npt.ArrayLike = None):
     iPLV = np.abs(np.imag(cPLV))
 
     return iPLV
+
 
 def get_PLV_mean(PLV):
     triu_idxs = np.triu_indices_from(PLV[:, :, 0], k=1)
